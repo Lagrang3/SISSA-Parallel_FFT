3ce4937fb9a7e69f354ae0ca20badcb6178b6590
Lagrang3 <lagrangelciencia@gmail.com>
2019-06-05 12:45:25 +0200

    in-place fft, first step towards 2D fft

--- solution/include/fft.h
+++ solution/include/fft.h
@@ -62,13 +62,12 @@ std::vector<T> FFT_DivideAndConquer(const std::vector<T>& A,const T e,const T _1
 }
 
 /*
-	Divide and Conquer algorithm to compute the Discrete Fourier Transform:
-	aka Fast Fourier Transform. 
-	!!! n must be a power of 2 and e must be and n-root of unity (_1)
+	In-place FFT
 */
-template<class T>
-std::vector<T> FFT_Iterative(const std::vector<T>& A,const T e,const T _1 = T(1)){
-	const int n=A.size();
+
+template<class iter, class T>
+void FFT(iter first,iter last,const T e,const T _1 = T(1)){
+	const int n=std::distance(first,last);
 	assert(__builtin_popcount(n)==1);
 	
 	T f = power(e,n/2,_1);
@@ -78,28 +77,43 @@ std::vector<T> FFT_Iterative(const std::vector<T>& A,const T e,const T _1 = T(1)
 		e2.push_back( e2.back()*e2.back() );
 	
 	std::reverse(e2.begin(),e2.end());
-	std::vector<T> B(A);
 	
-	for(int i=0;i<n;++i){
+	iter _i = first;
+	for(int i=0;i<n;++i,++_i){
 		int ib=i,j=0;
+		iter _j = first;
+		
 		for(int b=0;b<nbits;ib>>=1,++b)
-			j=(j<<1) | (ib&1) ;
+			j=(j<<1) | (ib&1);
+		
+		std::advance(_j,j);
+		
 		if(i<j)
-			std::swap(B[i],B[j]);
+			std::swap(*_i,*_j);
 	}
 	for(int len=2,k=1;len<=n;len<<=1,++k){
 		for(int i=0;i<n;i+=len){
 			T ej=_1;
 			for(int j=0;j<len/2;++j){
-				int u=i+j, v=i+j+len/2;
-				T Bu = B[u],Bv=B[v]*ej;
-				B[u]=Bu + Bv;
-				B[v]=Bu + Bv*f;
+				iter u=first+i+j, v=first+i+j+len/2;
+				T Bu = *u,Bv=*v*ej;
+				*u = Bu + Bv;
+				*v = Bu + Bv*f;
 				ej*=e2[k];
 			}
 		}
 	}	
-	
+}
+
+/*
+	Divide and Conquer algorithm to compute the Discrete Fourier Transform:
+	aka Fast Fourier Transform. 
+	!!! n must be a power of 2 and e must be and n-root of unity (_1)
+*/
+template<class T>
+std::vector<T> FFT_Iterative(const std::vector<T>& A,const T e,const T _1 = T(1)){
+	std::vector<T> B(A);
+	FFT(B.begin(),B.end(),e,_1);
 	return B;
 }
 
